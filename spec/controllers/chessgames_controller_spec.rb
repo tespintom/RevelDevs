4811ba2e31cbd8e677e35f74dadb7f004f5ac975
Joanna Tracy <joanna98146@yahoo.com>
2017-11-29 03:58:11 +0000

    added chessgames controler and tests

---
+++ spec/controllers/chessgames_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe ChessgamesController, type: :controller do
+   describe "chessgames#index action" do
+    it "should successfully show the page" do
+      expect(true).to be(true)
+    end
+   end
+end
