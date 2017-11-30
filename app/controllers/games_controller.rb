35b2c42e619fffce5ec57b31ad869bc49d4bcfd1
Elizabeth Hanson <emhanson08@gmail.com>
2017-11-30 00:17:46 +0000

    Updated the GamesController and View

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,25 @@
 class GamesController < ApplicationController
 
-def index
-  @games = Game.all
-end
+  def index
+    @games = Game.all
+  end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.create(game_params)
   end
 
   def show
+    @game = Game.find_by_id(params[:id])
+    render "Not found :(" if game.blank?
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :max_players, :result_id, :player_started_id)
   end
 end
