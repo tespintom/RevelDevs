6ec1920560e9ba31e2d2ae87839d49104c6f1880
Derek <you@example.com>
2017-12-16 23:27:16 +0000

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212233926) do
+ActiveRecord::Schema.define(version: 20171213233138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171212233926) do
     t.integer "total_players"
     t.bigint "white_player_id"
     t.bigint "black_player_id"
+    t.bigint "user_id"
   end
 
   create_table "pieces", force: :cascade do |t|
