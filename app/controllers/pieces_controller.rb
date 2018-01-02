56e733c8298253ff0a6f0c57d57050c35c6b6ef9
Elizabeth Hanson <emhanson08@gmail.com>
2018-01-02 02:55:24 +0000

    Removed references to user_id, which no longer exists. Added populate method back to game model. Created pieces controller show method.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,8 +2,19 @@ class PiecesController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @piece = Piece.find_by_id(params[:id])
+    render json: @piece
+    # @game = @piece.game
+    # redirect_to game_path(@game)
+    return render_not_found if @piece.blank?
   end
 
   def update
+  end
+
+  private
+
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 end
