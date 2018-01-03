89b280f0fc8dc367284ad7f78d20326fbfb0abec
JarfigNewton <jaredschil@gmail.com>
2018-01-03 21:15:30 +0000

    Alters pieces_controller_spec to hopefully fix Travis CI error

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe PiecesController, type: :controller do
     it 'should successfully return the piece if the piece is found' do
       game = FactoryBot.create(:game)
       #piece = game.pieces.active.where({x: 1, y: 1}).first
-      piece = FactoryBot.create(:piece, game_id: game.id, id: 1)
+      piece = FactoryBot.create(:piece, game_id: game.id)
       sign_in game.white_player
-      get :show, params: { id: 1 }
+      get :show, params: { id: piece.id }
       expect(response).to have_http_status :success
 
       # use todo app as reference for writing tests associated with JSON
