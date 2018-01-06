829694a0d304e68e432f6b68ea5cd003c9351fcc
Elizabeth Hanson <emhanson08@gmail.com>
2018-01-06 00:53:42 +0000

    Adjusts tests

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe PiecesController, type: :controller do
 
@@ -12,13 +13,18 @@ RSpec.describe PiecesController, type: :controller do
       expect(response_value['id']).to eq(piece.id)
     end
 
-    xit 'should successfully return the correct piece id if the piece is found' do
-      game = FactoryBot.create(:game)
+    it 'should successfully return the correct piece id if the piece is found' do
+      # game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = user.games.create(name: "game1")
       piece = game.pieces.active.where({x: 1, y: 2}).first
-      sign_in game.white_player
+      # sign_in game.user
       get :show, params: { id: piece.id }
       response_value = ActiveSupport::JSON.decode(@response.body)
+      binding.pry
       expect(response_value['id']).to eq(piece.id)
+      expect(response_value['color']).to eq("white")
       expect(response_value['type']).to eq("Pawn")
     end
 
