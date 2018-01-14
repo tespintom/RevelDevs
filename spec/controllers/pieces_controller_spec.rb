ebda90114d6815f1214cee96bab9fdcb1e2948e0
JarfigNewton <jaredschil@gmail.com>
2018-01-14 04:46:11 +0000

    Adds another test for pieces#update (invalid move case).

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 RSpec.describe PiecesController, type: :controller do
 
   describe 'pieces#show action' do
-    it 'should successfully return the correct piece id if the piece is found' do
+    it 'should have http status "success" if the piece is found' do
       game = FactoryBot.create(:game)
       piece = FactoryBot.create(:piece, game_id: game.id)
       sign_in game.user
@@ -44,20 +44,24 @@ RSpec.describe PiecesController, type: :controller do
   describe 'pieces#update action' do
     it 'should correctly update the piece\'s :x and :y if the move is valid' do
       game = FactoryBot.create(:game)
-      piece = game.pieces.active.where({x: 1, y: 2}).first
+      piece = game.pieces.active.find_by({x: 1, y: 2})
       sign_in game.user
-      put :update, params: { id: piece.id, piece: { x: 1, y: 3 } }
-      # response_value = ActiveSupport::JSON.decode(@response.body)
+      patch :update, params: { id: piece.id, piece: { x: 1, y: 3 } }
       expect(response).to have_http_status :success
       piece.reload
-      # expect(response_value['x']).to eq(1)
-      # expect(response_value['y']).to eq(3)
       expect(piece.x).to eq(1)
       expect(piece.y).to eq(3)
     end
 
-    xit 'should not update the piece\'s :x and :y if the move is not valid' do
-
+    it 'should not update the piece\'s :x and :y if the move is not valid' do
+      game = FactoryBot.create(:game)
+      piece = game.pieces.active.find_by({x: 1, y: 2})
+      sign_in game.user
+      patch :update, params: { id: piece.id, piece: { x: 1, y: 5 } }
+      expect(response).to have_http_status :success
+      piece.reload
+      expect(piece.x).to eq(1)
+      expect(piece.y).to eq(2)
     end
   end
 end
