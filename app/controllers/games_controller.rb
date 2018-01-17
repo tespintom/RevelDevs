efab6e15e0f1914b0fdb719c736c2cb921f27ec1
Abraham <apolishc@gmail.com>
2018-01-17 22:45:00 +0000

    Update games_controller.rb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    puts "O MY GOD, THEY KILLED KENNY!"
     @games = Game.all
     @available_games = @games.available
   end
